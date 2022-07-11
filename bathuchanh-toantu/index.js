9b54764538bfbe2d828020331554c4828504df63
vudichphuong <108725566+vudichphuong@users.noreply.github.com>
2022-07-11 11:53:08 +0700

    dd

---
+++ bathuchanh-toantu/index.js
@@ -0,0 +1,6 @@
+let inputWitdh = prompt(' Nhập chiều rộng ');
+let inputHeight = prompt('Nhập chiều dài');
+let witdh = parseInt(inputWitdh);
+let height = parseInt(inputHeight);
+let area =  witdh * height;
+document.write(' The area is: ' + area)
